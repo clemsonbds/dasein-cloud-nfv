2f11615481522a2497bb8d5f66f54d69a0fc8076
Udit Agarwal <udit1237@yahoo.com>
2015-08-14 12:44:24 -0400

    initial commit

---
+++ src/main/java/bds/clemson/nfv/workflow/info/ListVolumeProducts.java
@@ -0,0 +1,36 @@
+package bds.clemson.nfv.workflow.info;
+
+import java.util.Properties;
+
+import org.dasein.cloud.CloudException;
+import org.dasein.cloud.InternalException;
+import org.dasein.cloud.compute.VolumeProduct;
+
+import bds.clemson.nfv.exception.CapabilitiesException;
+import bds.clemson.nfv.exception.ConfigurationException;
+import bds.clemson.nfv.exception.ExecutionException;
+import bds.clemson.nfv.exception.ResourcesException;
+import bds.clemson.nfv.exception.UsageException;
+
+import bds.clemson.nfv.workflow.VolumeOperation;
+
+public class ListVolumeProducts extends VolumeOperation {
+
+		
+	protected void mapProperties(Properties prop) throws UsageException {
+	//none
+	}
+	
+	public static void main(String[] args) {
+		ListVolumeProducts operation = new ListVolumeProducts();
+		operation.execute();
+	}
+
+    protected void executeInternal() throws InternalException, CloudException, CapabilitiesException, ConfigurationException, ResourcesException, ExecutionException {
+    	super.executeInternal();
+
+        for (VolumeProduct product: volumeSupport.listVolumeProducts()) {
+        	System.out.println(product.getProviderProductId() + ", " + product.getName() + ", " + product.getDescription());
+        }
+    }
+}
