ff8a4dd8e9c313e70be16922de44cca86ab0f679
Jason Anderson <jwa2@clemson.edu>
2015-08-14 09:22:22 -0400

    Finish refactoring old operation classes.

--- src/main/java/bds/clemson/nfv/workflow/etsi/ListVirtualMachines.java
+++ src/main/java/bds/clemson/nfv/workflow/etsi/ListVirtualMachines.java
@@ -4,18 +4,16 @@ import java.util.Properties;
 
 import org.dasein.cloud.CloudException;
 import org.dasein.cloud.InternalException;
-import org.dasein.cloud.compute.ComputeServices;
 import org.dasein.cloud.compute.VirtualMachine;
-import org.dasein.cloud.compute.VirtualMachineSupport;
 
 import bds.clemson.nfv.exception.CapabilitiesException;
 import bds.clemson.nfv.exception.ConfigurationException;
 import bds.clemson.nfv.exception.ExecutionException;
 import bds.clemson.nfv.exception.ResourcesException;
 import bds.clemson.nfv.exception.UsageException;
-import bds.clemson.nfv.workflow.Operation;
+import bds.clemson.nfv.workflow.VMOperation;
 
-public class ListVirtualMachines extends Operation {
+public class ListVirtualMachines extends VMOperation {
 
 	protected void mapProperties(Properties prop) throws UsageException {
 		// none
@@ -27,18 +25,8 @@ public class ListVirtualMachines extends Operation {
 	}
 
     protected void executeInternal() throws InternalException, CloudException, CapabilitiesException, ConfigurationException, ResourcesException, ExecutionException {
-        // see if the cloud provider has any compute services
-        ComputeServices compute = provider.getComputeServices();
-
-        if( compute == null )
-            throw new CapabilitiesException(provider.getCloudName() + " does not support any compute services.");
-
-        // see if it specifically supports virtual machines
-        VirtualMachineSupport vmSupport = compute.getVirtualMachineSupport();
-
-        if( vmSupport == null )
-            throw new CapabilitiesException(provider.getCloudName() + " does not support virtual machines.");
-
+    	super.executeInternal();
+    	
         for( VirtualMachine vm : vmSupport.listVirtualMachines() ) {
             System.out.println(vm.getName() + " [" + vm.getProviderVirtualMachineId() + "] (" + vm.getCurrentState() + ")");
         }
--- src/main/java/bds/clemson/nfv/workflow/etsi/ListVmStorage.java
+++ src/main/java/bds/clemson/nfv/workflow/etsi/ListVmStorage.java
@@ -4,32 +4,19 @@ import java.util.Properties;
 
 import org.dasein.cloud.CloudException;
 import org.dasein.cloud.InternalException;
-import org.dasein.cloud.compute.ComputeServices;
 import org.dasein.cloud.compute.Volume;
-import org.dasein.cloud.compute.VolumeSupport;
 
 import bds.clemson.nfv.exception.CapabilitiesException;
 import bds.clemson.nfv.exception.ConfigurationException;
 import bds.clemson.nfv.exception.ExecutionException;
 import bds.clemson.nfv.exception.ResourcesException;
 import bds.clemson.nfv.exception.UsageException;
-import bds.clemson.nfv.workflow.Operation;
+import bds.clemson.nfv.workflow.VolumeOperation;
 
-public class ListVmStorage extends Operation {
+public class ListVmStorage extends VolumeOperation {
 	
 	protected void mapProperties(Properties prop) throws UsageException {
 		// none
-	}
-
-	protected void mapArguments(String[] args) {
-		// none
-	}
-	
-	protected void usage() {
-		System.out.println("usage: "
-				+ this.getClass().getName()
-				+ " <cloud name>"
-		);
 	}
 	
 	public static void main(String[] args) {
@@ -38,22 +25,11 @@ public class ListVmStorage extends Operation {
 	}
 
     protected void executeInternal() throws InternalException, CloudException, CapabilitiesException, ConfigurationException, ResourcesException, ExecutionException {
-        // see if the cloud provider has any compute services
-        ComputeServices compute = provider.getComputeServices();
+    	super.executeInternal();
 
-        if( compute == null )
-            throw new CapabilitiesException(provider.getCloudName() + " does not support any compute services.");
-
-        // see if it specifically supports volume support
-        VolumeSupport volSupport = compute.getVolumeSupport();
-
-        if( volSupport == null )
-            throw new CapabilitiesException(provider.getCloudName() + " does not support Volume.");
-
-         for(Volume vol:volSupport.listVolumes()){
-        System.out.println(vol.getName() + " [" + vol.getProviderVolumeId() + "] (" + vol.getCurrentState() + ")");
+    	for (Volume vol : volumeSupport.listVolumes()) {
+        	System.out.println(vol.getName() + " [" + vol.getProviderVolumeId() + "] (" + vol.getCurrentState() + ")");
         }
-       
     }
-  }
+}
 
