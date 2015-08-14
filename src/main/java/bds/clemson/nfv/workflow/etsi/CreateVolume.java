637ee5b762bc8a846b803ee4109ff2f956460da1
Udit Agarwal <udit1237@yahoo.com>
2015-08-14 12:41:07 -0400

    fixed minor errors

--- src/main/java/bds/clemson/nfv/workflow/etsi/CreateVolume.java
+++ src/main/java/bds/clemson/nfv/workflow/etsi/CreateVolume.java
@@ -4,10 +4,9 @@ import java.util.Properties;
 
 import org.dasein.cloud.CloudException;
 import org.dasein.cloud.InternalException;
-import org.dasein.cloud.compute.VmState;
 import org.dasein.cloud.compute.Volume;
 import org.dasein.cloud.compute.VolumeCreateOptions;
-import org.dasein.cloud.compute.VolumeProduct;
+import org.dasein.cloud.compute.VolumeState;
 import org.dasein.util.uom.storage.Gigabyte;
 import org.dasein.util.uom.storage.Storage;
 
@@ -21,10 +20,19 @@ import bds.clemson.nfv.workflow.VolumeOperation;
 
 public class CreateVolume extends VolumeOperation {
 
-	private String productName;
+	private String productId;
+	private String VolumeName;
+	private String VolumeDes;
+	private Integer capacity;
+    private Integer iops;
 	
 	protected void mapProperties(Properties prop) throws UsageException {
-		productName = Configuration.map(prop, "DSN_CMD_PRODUCT", Configuration.Requirement.REQUIRED);
+		productId = Configuration.map(prop, "DSN_CMD_PRODUCT", Configuration.Requirement.REQUIRED);
+		VolumeName = Configuration.map(prop, "DSN_CMD_VOLUMENAME", Configuration.Requirement.REQUIRED);
+		VolumeDes = Configuration.map(prop, "DSN_CMD_VOLUMEDES", Configuration.Requirement.REQUIRED);
+		capacity = Integer.parseInt(Configuration.map(prop, "DSN_CMD_CAPACITY", Configuration.Requirement.REQUIRED));
+		iops = Integer.parseInt(Configuration.map(prop, "DSN_CMD_IOPS", Configuration.Requirement.REQUIRED));
+		
 	}
 	
 	public static void main(String[] args) {
@@ -34,34 +42,15 @@ public class CreateVolume extends VolumeOperation {
 
     protected void executeInternal() throws InternalException, CloudException, CapabilitiesException, ConfigurationException, ResourcesException, ExecutionException {
     	super.executeInternal();
-    	
-        // launch a vm
-    	/**
-                     * Provides options for creating a block volume of a specific size based on a specific product. This method makes no sense
-                     * when {@link VolumeSupport#getVolumeProductRequirement()}  is {@link Requirement#NONE}.
-                     * @param volumeProductId the ID of the volume product from {@link VolumeSupport#listVolumeProducts()} to use in creating the volume
-                     * @param size the size of the volume to be created (or to be resized to)
-                     * @param name the name of the new volume
-                     * @param description a friendly description of the purpose of the new volume
-                     * @param iops the minimum guaranteed iops or 0 if no guarantees are sought
-                     * @return an object representing the options for creating the new volume
- **/
-        
-        // get product from productName first? this will be null
-		Storage<Gigabyte> size = new Storage<Gigabyte>(2, Storage.GIGABYTE); // how to specify size, what would be the format
-        String volname="test";
-        String voldescription="might work";
-        int voliops=300;
-
-        VolumeCreateOptions options = VolumeCreateOptions.getInstance(productName, size, volname, voldescription, voliops);
-
-        
-                          
-        String volumeId = volumeSupport.createVolume(options); // NOt sure whether to change to string or change to volume
+         
+      
+		Storage<Gigabyte> size = new Storage<Gigabyte>(capacity, Storage.GIGABYTE); 
+        VolumeCreateOptions options = VolumeCreateOptions.getInstance(productId, size, VolumeName, VolumeDes, iops);
+        String volumeId = volumeSupport.createVolume(options); 
         Volume vol = volumeSupport.getVolume(volumeId);
 
         System.out.println("Created: " + vol.getName() + "[" + vol.getProviderVolumeId() + "] (" + vol.getCurrentState() + ")");
-        while( vol != null && vol.getCurrentState().equals(VmState.PENDING) ) {
+        while( vol != null && vol.getCurrentState().equals(VolumeState.PENDING) ) {
             System.out.print(".");
             try { Thread.sleep(5000L); }
             catch( InterruptedException ignore ) { }
